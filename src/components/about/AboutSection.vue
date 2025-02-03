782ec45ac49d0a938737d37f0b53e83f50e89eed
Josh Humphrey <joshhumphrey@Joshs-Laptop.local>
2025-02-03 13:29:07 -0600

    Added button to download resume

--- src/components/about/AboutSection.vue
+++ src/components/about/AboutSection.vue
@@ -1,11 +1,25 @@
 <script setup>
 // Components
-
+import Button from '../UI/Button.vue'
 // Assets
 import github from '@/assets/github-mark.svg'
 import linkedin from '@/assets/linkedin.svg'
+import resume from '@/assets/resume.pdf'
+
 // const props = defineProps({})
 // const emit = defineEmits()
+
+const downloadResume = () => {
+    const link = document.createElement('a')
+    link.href = resume
+    link.target = '_blank'
+    link.download = 'Joshua Humphrey Resume.pdf' // Name of the file to be downloaded
+
+    // Simulate a click on the element <a>
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+}
 </script>
 
 <template>
@@ -15,7 +29,7 @@ import linkedin from '@/assets/linkedin.svg'
         <div class="w-full gap-2 flex-col-ic-jc">
             <p class="text-2xl text-center">Joshua Humphrey</p>
             <p class="text-lg text-center">Fullstack Developer</p>
-            <p class="w-2/3 text-center font-subtext">
+            <p class="w-10/12 text-center md:w-2/3 font-subtext">
                 Passionate about writing clean code and eating good meals.
             </p>
         </div>
@@ -34,5 +48,10 @@ import linkedin from '@/assets/linkedin.svg'
                 ><img :src="linkedin" alt="" class="size-8 md:size-10"
             /></a>
         </div>
+        <Button
+            @click="downloadResume"
+            button-class="hover:mt-[1px]"
+            text="Get Resume"
+        />
     </section>
 </template>
